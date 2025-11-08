5bf00945e1222a0dbaa21cd34a235f12549f7517
Joe O'Brien <joseph.obrien4@snhu.edu>
2025-11-08 01:44:12 -0500

    Module 2 completed baseline

--- app.js
+++ app.js
@@ -12,8 +12,10 @@ var handlebars = require('hbs');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+
 app.set('view engine', 'hbs');
+
 
 handlebars.registerPartials(__dirname + '/app_server/views/partials');
 
