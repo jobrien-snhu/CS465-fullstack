075dd556c831e020ac0b6ec45d6f4dd08209ae10
Joe O'Brien <joseph.obrien4@snhu.edu>
2025-12-17 08:27:05 -0500

    Final Module 7 updates

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ var apiRouter = require('./app_api/routes/index');
 
 var handlebars = require('hbs');
 
+// Wire in our authentication module
+var passport = require('passport');
+require('./app_api/config/passport');
+
+
 //Bring in the database
 require('./app_api/models/db');
+
+//Bring in .env file variables
+require('dotenv').config();
 
 var app = express();
 
@@ -29,11 +37,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+// Catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({"message": err.name + ": " + err.message});
+  }
+});
+
 
 // Enable CORS
 app.use('/api', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     next();
 });
